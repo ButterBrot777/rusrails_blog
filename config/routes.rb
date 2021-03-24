1654e466f6a61b9f7ecdab2effad4fefc5154538
Pavel Karenda <korendos@gmail.com>
2021-03-24 12:55:01 +0300

    feat: create comments mvc

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get 'articles/new'
   get 'articles/show'
 
-  resources :articles
+  resources :articles do
+	  resources :comments
+  end
   
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
