2ce4b6a9d21e9056bd66e69543ab068564380c7c
Pavel Karenda <korendos@gmail.com>
2021-03-25 17:36:55 +0300

    feat: add concern

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'articles/new'
+  get 'articles/show'
+
+  resources :articles do
+    resources :comments
+  end
+
+  # root 'welcome#index'
+  root 'articles#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
