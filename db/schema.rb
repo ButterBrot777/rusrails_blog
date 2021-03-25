8ff680ac9fc810d9a442dfda5830a1cf03e1b004
Pavel Karenda <korendos@gmail.com>
2021-03-25 11:43:53 +0300

    try: implement Concern(not working)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_134314) do
+ActiveRecord::Schema.define(version: 2021_03_24_151941) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
